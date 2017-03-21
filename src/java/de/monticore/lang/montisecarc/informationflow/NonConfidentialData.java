362429e98a0f5bf3e9d3ed1af3871eb5845e83ff
Thomas Buning <thomas.buning@rwth-aachen.de>
2017-03-21 09:14:25 +0100

    Initialize Information Flow Control Project

---
+++ src/java/de/monticore/lang/montisecarc/informationflow/NonConfidentialData.java
@@ -0,0 +1,6 @@
+package de.monticore.lang.montisecarc.informationflow;
+
+
+@SecurityClassification(securityClass = MSASecurityClass.NON_CONFIDENTIAL)
+public class NonConfidentialData {
+}
---
+++ src/java/de/monticore/lang/montisecarc/informationflow/TopSecretData.java
@@ -0,0 +1,6 @@
+package de.monticore.lang.montisecarc.informationflow;
+
+
+@SecurityClassification(securityClass = MSASecurityClass.TOP_SECRET)
+public class TopSecretData {
+}
